90f2f471a81e12887791f8998d911d11b03f1d9a
krosi <krosidigital@gmail.com>
2025-07-24 11:16:05 +0400

    front3 Done

---
+++ codingbat-solutions/Warmup-1/front3.java
@@ -0,0 +1,7 @@
+public class front3 {
+    public String front3(String str) {
+        String front = str.length() < 3 ? str : str.substring(0, 3);
+        return front + front + front;
+
+    }
+}
