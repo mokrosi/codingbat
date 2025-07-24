e5784ba1f45b0089628b3f3a201edc7f47d2a0ed
krosi <krosidigital@gmail.com>
2025-07-24 09:54:34 +0400

    makes10 Done

---
+++ codingbat-solutions/Warmup-1/makes10.java
@@ -0,0 +1,8 @@
+public class makes10 {
+    public boolean makes10(int a, int b) {
+        if ((a == 10 || b == 10) || ((a + b) == 10)) {
+            return true;
+        }
+        return false;
+    }
+}
