4b5451470bb791a353879fd552b923a704c751ea
krosi <krosidigital@gmail.com>
2025-07-24 09:53:15 +0400

    parrotTrouble Done

---
+++ codingbat-solutions/Warmup-1/parrotTrouble.java
@@ -0,0 +1,10 @@
+public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+
+        if (talking && (hour < 7 || hour > 20)) {
+            return true;
+        }
+        return false;
+
+    }
+}
